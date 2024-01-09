e5e55dae047a42cbdb411bf3bfee985a66ced99b
mi-ward <mwardcoding+gh@gmail.com>
2024-01-09 00:21:21 -0500

    completed 275

--- 4.5 - How To Design Programs Book/5. Abstraction/exercise_275.rkt
+++ 4.5 - How To Design Programs Book/5. Abstraction/exercise_275.rkt
@@ -2,12 +2,13 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname exercise_275) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;Exercise 275
+(require 2htdp/batch-io)
 
 (define LETTERS
   (explode "abcdefghijklmnopqrstuvwxyz"))
 
 (define LOCATION "/usr/share/dict/words")
-;(define AS-List (read-lines LOCATION))
+(define AS-List (read-lines LOCATION))
 
 (define-struct lc [letter count])
 
@@ -33,4 +34,33 @@
     (first (sort (map convert-to-lc LETTERS) lc>))))
 
 (check-expect (most-frequent DICT1) (make-lc "b" 2))
-  
+(check-expect (most-frequent AS-List) (make-lc "s" 22759))
+
+;Dictionary -> List-of Dictionary
+; creates a list of dictionaries, one per letter
+(define (words-by-first-letter dict)
+  (local [(define (generate-words-per-letter letter los)
+            (cond [(empty? los) '()]
+                  [else
+                   (if (string=? letter (substring (first los) 0 1))
+                       (cons (first los) (generate-words-per-letter letter (rest los)))
+                       (generate-words-per-letter letter (rest los)))]))
+          (define (generate-dict letter)
+            (generate-words-per-letter letter dict))
+          (define (full? lst)
+            (not (empty? lst)))]
+    (filter full? (map generate-dict LETTERS))))
+
+(check-expect (words-by-first-letter DICT1) (list
+                                             (list "apple")
+                                             (list "banana" "blueberry")
+                                             (list "fruit")
+                                             (list "kiwi")
+                                             (list "lemon")
+                                             (list "orange")
+                                             (list "pineapple")
+                                             (list "strawberry")))
+          
+          
+
+
